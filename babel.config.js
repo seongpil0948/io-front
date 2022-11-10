ff00de8e532fd39c9fa7471b4cd5902978a3132f
seongpil0948 <seongpil0948@gmail.com>
2022-11-10 23:35:02 +0900

    babel exclude node_modules

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ["@vue/cli-plugin-babel/preset"],
+  overrides: [
+    {
+      test: /\.js$/,
+      compact: false,
+    },
+    {
+      test: "**/node_modules/**",
+      compact: false,
+    },
+  ],
+  // loaders: [{ test: /\.js$/, loader: "babel", query: { compact: false } }],
 };
