bd781eea03d7a7cf9161df0343faad690caa47ed
seongpil0948 <seongpil0948@gmail.com>
2022-11-11 06:07:30 +0900

    set up test

--- babel.config.js
+++ babel.config.js
@@ -9,6 +9,10 @@ module.exports = {
       test: "**/node_modules/**",
       compact: false,
     },
+    {
+      test: "**/administrationAreas.ts",
+      compact: false,
+    },
   ],
   // loaders: [{ test: /\.js$/, loader: "babel", query: { compact: false } }],
 };
