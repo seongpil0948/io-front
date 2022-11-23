0b4e6d506c2c07286f0344dbd4f93ab229df37b4
seongpil0948 <seongpil0948@gmail.com>
2022-11-24 07:15:15 +0900

    for print test

--- src/component/button/VendorCompletePayButton.vue
+++ src/component/button/VendorCompletePayButton.vue
@@ -4,6 +4,7 @@ import { logger } from "@/plugin/logger";
 import { useAuthStore } from "@/store";
 import { formatDate, getUserName, IoUser, USER_DB } from "@io-boxies/js-lib";
 import { makeMsgOpt, useAlarm } from "@io-boxies/vue-lib";
+import { Instagram } from "@vicons/fa";
 import { NButton, useMessage } from "naive-ui";
 import { Size, Type } from "naive-ui/es/button/src/interface";
 import { ref, toRefs, watchEffect } from "vue";
@@ -76,7 +77,20 @@ async function selectPrinter() {
   console.log("selectPrinter");
 }
 async function onPrint() {
-  console.log("onPrint");
+  console.log("onPrint ");
+  const card = document.getElementById("receipt-card");
+  const html = document.querySelector("html");
+  if (!card || !html) return msg.error("다시 시도 해주세요.");
+  const printContents = card.innerHTML;
+  const printDiv = document.createElement("DIV");
+  printDiv.className = "print-div";
+
+  html.appendChild(printDiv);
+  printDiv.innerHTML = printContents;
+  document.body.style.display = "none";
+  window.print();
+  document.body.style.display = "block";
+  printDiv.style.display = "none";
 }
 </script>
 
@@ -96,16 +110,72 @@ async function onPrint() {
     size="huge"
   >
     <n-space justify="space-around">
-      <n-card style="width: 45vw; height: 80vh; overflow: auto">
+      <n-card id="receipt-card">
         <n-space vertical align="end">
           <n-text> {{ formatDate(new Date(), "MIN") }}</n-text>
           <n-text v-if="targetShop">
             {{ getUserName(targetShop) }} 귀하
           </n-text>
         </n-space>
-        <n-divider />
+        <n-divider class="black-divider" />
         <n-h2 class="center-txt">{{ getUserName(auth.currUser) }}</n-h2>
         <n-h4 class="center-txt">주문 영수증</n-h4>
+        <n-divider class="black-divider" />
+        <n-space vertical align="start" item-style="width: 100%">
+          <n-text>hp: 010-7184-0948</n-text>
+          <n-text>tel: 010-7727-7428</n-text>
+          <n-text>kakao: fuckjunhai</n-text>
+          <n-text><n-icon :component="Instagram" />: fuckjunhai</n-text>
+          <n-text
+            >메모: <br />메모 메모 항헹홍행홍 <br />메모 메모 항헹홍행홍
+            <br />메모 메모 항헹홍행홍
+          </n-text>
+          <n-space vertical align="center">
+            <img
+              style="width: 20vw"
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png"
+            />
+            <table>
+              <thead>
+                <tr>
+                  <th colspan="2">The table header</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>The table body</td>
+                  <td>with two columns</td>
+                </tr>
+                <tr>
+                  <td>The table body</td>
+                  <td>with two columns</td>
+                </tr>
+                <tr>
+                  <td>The table body</td>
+                  <td>with two columns</td>
+                </tr>
+              </tbody>
+            </table>
+          </n-space>
+
+          <n-divider class="black-divider" />
+          <n-space justify="space-between">
+            <n-text type="info">수량합계</n-text><n-text>15</n-text>
+          </n-space>
+          <n-space justify="space-between">
+            <n-text type="info">부가세</n-text><n-text>5,000</n-text>
+          </n-space>
+          <n-space justify="space-between">
+            <n-text type="info">금액 합계</n-text><n-text>75,000</n-text>
+          </n-space>
+          <n-space justify="space-between">
+            <n-text type="info">미결제 금액</n-text><n-text>120,000</n-text>
+          </n-space>
+          <n-space justify="space-between">
+            <n-text type="info">총 결제 금액</n-text><n-text>200,000</n-text>
+          </n-space>
+          <n-divider class="black-divider" />
+        </n-space>
       </n-card>
       <n-space vertical>
         <n-space>
@@ -125,3 +195,14 @@ async function onPrint() {
     </template>
   </n-modal>
 </template>
+
+<style>
+#receipt-card {
+  width: 45vw;
+  height: 80vh;
+  overflow: auto;
+}
+.black-divider {
+  border-top: 1px solid black;
+}
+</style>
