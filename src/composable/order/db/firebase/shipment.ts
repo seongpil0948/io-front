7f34bbfddbd2c96042724e437b8bbfd2e16a261d
seongpil0948 <seongpil0948@gmail.com>
2022-12-13 19:18:23 +0900

    엉클관리자 픽업승인 안돼서 완전한 테스트가

--- src/composable/order/db/firebase/shipment.ts
+++ src/composable/order/db/firebase/shipment.ts
@@ -8,7 +8,7 @@ import {
   uncleAvailShip,
   VENDOR_GARMENT_DB,
 } from "@/composable";
-import { getIoCollection, IoCollection } from "@io-boxies/js-lib";
+import { getIoCollection, IoCollection, USER_DB } from "@io-boxies/js-lib";
 import { uuidv4 } from "@firebase/util";
 import { IoUser, userFireConverter } from "@io-boxies/js-lib";
 import { doc, runTransaction } from "firebase/firestore";
@@ -25,34 +25,18 @@ export const ShipmentFB: ShipDB<IoOrder> = {
       throw new Error("보유 코인이 부족합니다.");
     const ordRef = getOrdRef(row.shopId);
     const ordDocRef = doc(ordRef, row.dbId).withConverter(converterGarment);
-    return runTransaction(ioFire.store, async (transaction) => {
-      const ordDoc = await transaction.get(ordDocRef);
+    return runTransaction(ioFire.store, async (t) => {
+      // >>> read order
+      const ordDoc = await t.get(ordDocRef);
       if (!ordDoc.exists()) throw new Error("order doc does not exist!");
-
       const ord = ordDoc.data()!;
       if (ord.items.length < 1)
         throw new Error("request order items not exist");
 
-      const uncleDoc = await transaction.get(
-        doc(
-          getIoCollection({ c: IoCollection.USER }).withConverter(
-            userFireConverter
-          ),
-          ord.shipManagerId
-        )
-      );
-      const shopDoc = await transaction.get(
-        doc(
-          getIoCollection({ c: IoCollection.USER }).withConverter(
-            userFireConverter
-          ),
-          ord.shopId
-        )
-      );
-
+      const uncleDoc = await t.get(doc(uConverter, ord.shipManagerId));
+      const shopDoc = await t.get(doc(uConverter, ord.shopId));
       const uncle = validateUser(uncleDoc.data(), ord.shipManagerId!);
       const shop = validateUser(shopDoc.data(), ord.shopId);
-
       for (let i = 0; i < ord.items.length; i++) {
         const item = ord.items[i];
         const prod = await VENDOR_GARMENT_DB.getById(
@@ -63,27 +47,17 @@ export const ShipmentFB: ShipDB<IoOrder> = {
           throw new Error(
             `도매처 상품이 없습니다.: ${item.vendorProd.vendorProdId}`
           );
-        const vendorRef = await transaction.get(
-          doc(
-            getIoCollection({ c: IoCollection.USER }).withConverter(
-              userFireConverter
-            ),
-            item.vendorId
-          )
-        );
-        const vendor = validateUser(vendorRef.data(), item.vendorId);
+        const vendorUser = await USER_DB.getUserById(item.vendorId);
+        const vendor = validateUser(vendorUser, item.vendorId);
         const isReturn = item.orderType === "RETURN";
         const shopLocate = shop.companyInfo!.shipLocate;
         const vendorLocate = vendor.companyInfo!.shipLocate;
-        if (!shopLocate) {
-          return new Error(
-            `소매처 대표 배송지 정보가 없습니다. ${shop.userInfo.userId}`
-          );
-        } else if (!vendorLocate) {
-          return new Error(
-            `도매처 대표 배송지 정보가 없습니다. ${shop.userInfo.userId}`
-          );
-        }
+        const shopId = shop.userInfo.userId;
+        if (!shopLocate)
+          return new Error(`소매처 대표 배송지 정보가 없습니다. ${shopId}`);
+        else if (!vendorLocate)
+          return new Error(`도매처 대표 배송지 정보가 없습니다. ${shopId}`);
+
         const clientPickL = isReturn ? shopLocate : vendorLocate;
         const clientshipL = isReturn ? vendorLocate : shopLocate;
         const clientshipLStr =
@@ -119,7 +93,7 @@ export const ShipmentFB: ShipDB<IoOrder> = {
           wishedDeliveryTime: new Date(),
           managerId: uncle.userInfo.userId,
         });
-        transaction.set(
+        t.set(
           doc(
             getIoCollection({
               c: IoCollection.SHIPMENT,
@@ -132,14 +106,15 @@ export const ShipmentFB: ShipDB<IoOrder> = {
         item.shipmentId = shipment.shippingId;
         setState(ord, item.id, "BEFORE_ASSIGN_PICKUP");
       }
-      transaction.update(ordDocRef, converterGarment.toFirestore(ord));
-      transaction.update(
+      t.update(ordDocRef, converterGarment.toFirestore(ord));
+      t.update(
         doc(getIoCollection({ c: IoCollection.IO_PAY }), userPay.userId),
         {
           pendingBudget: userPay.pendingBudget + expectedReduceCoin,
           budget: userPay.budget - expectedReduceCoin,
         }
       );
+
       return ord;
     });
   },
@@ -166,3 +141,6 @@ export function validateUser(
     throw new Error("엉클 배송지와 요금 설정을 해주세요");
   return u!;
 }
+const uConverter = getIoCollection({ c: IoCollection.USER }).withConverter(
+  userFireConverter
+);
