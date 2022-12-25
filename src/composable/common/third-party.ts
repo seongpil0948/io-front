2d3a36859d12abd00aca8c0beff0f742a45e6bda
seongpil0948 <seongpil0948@gmail.com>
2022-12-26 00:00:07 +0900

    third-party

---
+++ src/composable/common/third-party.ts
@@ -0,0 +1,31 @@
+import { useAuthStore } from "@/store";
+import { useAlarm } from "@io-boxies/vue-lib";
+import { useMessage } from "naive-ui";
+import { computed } from "vue";
+import { useLogger } from "vue-logger-plugin";
+import { axiosConfig } from "@/plugin/axios";
+import { makeMsgOpt, isMobile } from "@/util";
+import { uuidv4 } from "@firebase/util";
+
+export function commonThird() {
+  const msg = useMessage();
+  const log = useLogger();
+  const auth = useAuthStore();
+  const u = auth.user ?? auth.currUser;
+  const uid = computed(() => auth.currUser.userInfo.userId);
+  const smtp = useAlarm();
+  const apiBaseUrl = axiosConfig.baseURL;
+
+  return {
+    msg,
+    log,
+    auth,
+    u,
+    uid,
+    smtp,
+    apiBaseUrl,
+    makeMsgOpt,
+    isMobile,
+    uuidv4,
+  };
+}
