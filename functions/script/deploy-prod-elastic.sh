91e7d1f403b9293688a015c8ae1323386055d86f
seongpil0948 <seongpil0948@gmail.com>
2023-01-28 17:04:23 +0900

    apply search functions

--- functions/script/deploy-prod-elastic.sh
+++ functions/script/deploy-prod-elastic.sh
@@ -1,6 +1,6 @@
-echo "ELASTIC_DEV_USER_NAME: $ELASTIC_DEV_USER_NAME" 
-echo "ELASTIC_DEV_PASSWORD: $ELASTIC_DEV_PASSWORD" 
-echo "ELASTIC_DEV_END_POINT: $ELASTIC_DEV_END_POINT" 
+echo "ELASTIC_USER_NAME: $ELASTIC_USER_NAME" 
+echo "ELASTIC_PASSWORD: $ELASTIC_PASSWORD" 
+echo "ELASTIC_END_POINT: $ELASTIC_END_POINT" 
 # echo "ELASTIC_VENDOR_PROD_INDEX: $ELASTIC_VENDOR_PROD_INDEX" 
 firebase use io-box
 firebase projects:list
@@ -10,9 +10,9 @@ if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
 then
     echo "gogo"
     firebase functions:config:set \
-        elasticsearch.username=$ELASTIC_DEV_USER_NAME \
-        elasticsearch.password=$ELASTIC_DEV_PASSWORD \
-        elasticsearch.url=$ELASTIC_DEV_END_POINT \
+        elasticsearch.username=$ELASTIC_USER_NAME \
+        elasticsearch.password=$ELASTIC_PASSWORD \
+        elasticsearch.url=$ELASTIC_END_POINT \
         # elasticsearch.vendor_prod_index=$ELASTIC_VENDOR_PROD_INDEX
 
     # firebase deploy --only functions:scheduledElasticHealthCheck,functions:elasticVendorProdSearch
